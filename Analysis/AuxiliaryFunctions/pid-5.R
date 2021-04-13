8f0a63c38aaf29f2c3123615964dee913c78fd46
Iair <iair.embon@gmail.com>
2021-04-13 16:33:56 -0300

    seguir funcion pid5

--- Analysis/AuxiliaryFunctions/pid-5.R
+++ Analysis/AuxiliaryFunctions/pid-5.R
@@ -1,45 +1,24 @@
 ### get the PID-5 score
 
-puntaje_pid <- function (cant_sujetos,cant_componentes_por_sujetos,ubicacion_comp_AQ){
-  # cant_sujetos = cantidad de sujetos; cant_componentes_por_sujetos = cantidad de componentes en un sujeto;
-  # ubicacion_comp_AQ = la ubicacion del componente AQ en el primer sujeto
+puntaje_pid <- function (cant_sujetos,cant_componentes_por_sujetos,ubicacion_comp1_pid){
+  # cant_sujetos = cantidad de sujetos; 
+  # cant_componentes_por_sujetos = cantidad de componentes en un sujeto;
+  # ubicacion_comp_pid = la ubicacion del primer componente pid en el primer sujeto
   
-  # inicializo la variable que va a contener los puntajes del AQ para cada sujeto
-  puntaje_AQ_sujetos <- rep(NA,cant_sujetos)
+  # res[[4]] primera parte
+  # res[[5]] segunda parte
+  # 1 = Muy falso o a menudo falso
+  # 2 = A veces o algo falso
+  # 3 = A veces o algo verdadero
+  # 4 = Muy cierto o a menudo verdadero
   
-  # para cada sujeto, que saque el puntaje de AQ
-  for (s in 1:cant_sujetos){
-    
-    # creo una variable con las respuestas del participante
-    respuestas_AQ <- AQ[ubicacion_comp_AQ]$value
-
-    # Modifico la ubicacion de las respuestas del AQ para el siguiente sujeto
-    ubicacion_comp_AQ <- ubicacion_comp_AQ + cant_componentes_por_sujetos
-    
-    # inicializo variable que va a contener el puntaje del participante
-    puntaje_AQ <- 0 
-    
-    # Saco el puntaje del participante de acuerdo a sus respuestas. (hasta el indice 25 (inclu?do) total acuerdo 
-    # y acuerdo parcial puntea 1. Desde el ?ndice 26 hasta el 40, desacuerdo total y desacuerdo parcial puntea 1
-    for (i in 1:length(respuestas_AQ)){
-      if (i <= 25){
-        if(respuestas_AQ[i]== 1){
-          puntaje_AQ <- puntaje_AQ + 1
-        }else if (respuestas_AQ[i]== 2){
-          puntaje_AQ <- puntaje_AQ + 1
-        }
-      }else {
-        if(respuestas_AQ[i]==4){
-          puntaje_AQ <- puntaje_AQ + 1
-        }else if (respuestas_AQ[i]==3){
-          puntaje_AQ <- puntaje_AQ + 1
-        }
-      }
-    } 
-    # Completo el puntaje del sujeto en su lugar correspondiente
-    puntaje_AQ_sujetos[s] <- puntaje_AQ
+  puntaje.sujetos <- rep(NaN, cant_sujetos)
+  
+  # puntar un solo sujeto
+  for (i in 1:220) {
+    parte1 <- res[[ubicacion_comp1_pid]]$value
+    parte2 <- res[[5]]$value
   }
-  # Le pido que me devuelva un vector con los puntajes de cada sujeto en orden 
-  return(puntaje_AQ_sujetos)
+
 }
 
