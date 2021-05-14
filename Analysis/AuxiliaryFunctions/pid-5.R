398a9652343c9799886156eb22a71da91ec3e442
Iair Embon <iair.embon@gmail.com>
2021-05-14 16:48:07 -0300

    se corrigió una función, pid5

--- Analysis/AuxiliaryFunctions/pid-5.R
+++ Analysis/AuxiliaryFunctions/pid-5.R
@@ -133,11 +133,11 @@ puntaje_pid <- function (cant_sujetos,ubicacion_respuestas_pid1,ubicacion_respue
     scoreWithdrawal                      <- mean(puntaje.sujeto.cuestion[indsWithdrawal])
     
     
-    ScoreDomainNegativeAffect <- mean(scoreEmotionalLability,scoreAnxiousness, scoreSeparationInsecurity)
-    ScoreDomainDetachment     <- mean(scoreWithdrawal, scoreAnhedonia, scoreIntimacyAvoidance)
-    ScoreDomainAntagonism     <- mean(scoreManipulativeness, scoreDeceitfulness, scoreGrandiosity)
-    ScoreDomainDisinhibition  <- mean(scoreIrresponsibility, scoreImpulsivity, scoreDistractivility)
-    ScoreDomainPsychoticism   <- mean(scoreUnusualBeliefsAndExperiences, scoreExcentricity, scorePerceptualDysregulation)
+    ScoreDomainNegativeAffect <- mean(c(scoreEmotionalLability,scoreAnxiousness, scoreSeparationInsecurity))
+    ScoreDomainDetachment     <- mean(c(scoreWithdrawal, scoreAnhedonia, scoreIntimacyAvoidance))
+    ScoreDomainAntagonism     <- mean(c(scoreManipulativeness, scoreDeceitfulness, scoreGrandiosity))
+    ScoreDomainDisinhibition  <- mean(c(scoreIrresponsibility, scoreImpulsivity, scoreDistractivility))
+    ScoreDomainPsychoticism   <- mean(c(scoreUnusualBeliefsAndExperiences, scoreExcentricity, scorePerceptualDysregulation))
     
     # guarda puntaje de todos los sujetos
     scoreAnhedonia.Completo[i]                       <- scoreAnhedonia
