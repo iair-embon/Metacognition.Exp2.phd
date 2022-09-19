043cb7fd54af1b33a4063a5e1d28c32cbc64fcad
iair-embon <iair.embon@gmail.com>
2022-09-19 16:08:10 -0300

    finish boostrap analysis for 0 probability in elasticNet regression, with alpha and lambda selected by LOOCV

--- git/Analysis/RegressionAnalysis/Conf_PID_facets_elasticNet_alpha_selection.R
+++ git/Analysis/RegressionAnalysis/Conf_PID_facets_elasticNet_alpha_selection.R
@@ -46,8 +46,8 @@ m_elnet  = train(
   tuneLength = 50
 )
 
-alpha_selected <- m_elnet$bestTune[[1]]
-lambda_selected <- m_elnet$bestTune[[2]]
+alpha_selected <- m_elnet$bestTune[[1]] # 0.2285714
+lambda_selected <- m_elnet$bestTune[[2]] # 0.1555395
 
 ## runing the model regression with the selected alpha and lambda
 
@@ -80,14 +80,12 @@ print(fit)
 a <- coef(fit)
 
 
-### now, I perform boostrap to know the p-value of the not zero
-# predictors coefficients
-
-non_zero_predict <- a[a[,1]!=0,]
+### now, I perform boostrap to know the CI and the proportion of times that the 
+### predictor was exactly 0.
 
 boot_fun <- function(data, indices, alpha_selected , lambda_selected, ConfMean){
   boot_data <- data[indices,]
-
+  
   boot_matrix <- boot_data %>%
     select(!c(Participant,
               DomainNegativeAffect,
@@ -110,35 +108,41 @@ boot_fun <- function(data, indices, alpha_selected , lambda_selected, ConfMean){
          alpha = alpha_selected,
          lambda = lambda_selected)
   
-  coef_fit_boot <- coef(fit_boot)[12]
+  coef_fit_boot <- coef(fit_boot) 
+  c <- c(coef_fit_boot[1:length(coef_fit_boot)]) 
   
-  return(coef_fit_boot)
+  return(c) 
   }
 
-
-list_rep_boot <- list()
-i<- length(a[a[,1]!=0,])
-while(i > 0) {
-  
-  rep_boot <- boot(
+rep_boot <- boot(
     data = d,
     statistic = boot_fun,
-    R = 2000, 
+    R = 5000, 
     alpha_selected = alpha_selected,
     lambda_selected = lambda_selected,
-    ConfMean = d$ConfMean,
-    predictor = non_zero_predict[[i]]
+    ConfMean = d$ConfMean
   )
-  
-  
-  list_rep_boot <- list(list_rep_boot, rep_boot)
-  
-  i <- i - 1
-}
 
+boot.ci(rep_boot, index = 14) # se puede ir variando el index
 
-rep_boot
+df_rep_boot <- data.frame(predictor = character(0), ### es posible que deba borrar todo este df
+                          coeffiient = numeric(0),
+                          mean_boot = numeric(0),
+                          std_error_boot = numeric(0),
+                          LCI = numeric(0),
+                          UCI = numeric(0),
+                          prop_zero = numeric(0))  
 
-plot(rep_boot)
+# probability of 0 (page 154, Statistical learning with sparsity... Hastie)
+sum(rep_boot$t[,12] == 0)/5000
+sum(rep_boot$t[,13] == 0)/5000
+sum(rep_boot$t[,14] == 0)/5000
 
-boot.ci(rep_boot, type="bca")
+save(rep_boot, file = "git/Data/Regression_Results/Conf_PID_domain_Boot_elasticNet.RData")
+
+hist(rep_boot$t[,13])
+mean(rep_boot$t[,14])
+plot(rep_boot, index = 2)
+plot(rep_boot, index = 12)
+plot(rep_boot, index = 13)
+plot(rep_boot, index = 14)
