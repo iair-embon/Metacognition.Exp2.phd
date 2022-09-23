5c681cb572c697b5ef208cc205d76f9543c0c79f
Iair <iair.embon@gmail.com>
2022-09-23 16:57:32 -0300

    NC

---
+++ git/Figures/Code/Conf_PID_facets_elasticNet_alpha_selection.R
@@ -0,0 +1,221 @@
+### ElasticNet linear model with Confidence facets - alpha selection
+
+# libraries
+library(glmnet)
+library(dplyr)
+library(caret)
+library(boot)
+
+
+root <- rprojroot::is_rstudio_project
+basename(getwd())               
+
+####### data frame with filters already applied
+filepath <- root$find_file("git/Data/df_total_filtered.Rda")
+load(file= filepath)
+
+source(root$find_file("git/Analysis/AuxiliaryFunctions/DataFrame_subset.R"))
+d <- DataFrame_subset(df_total)
+
+### preprocessing
+d$gender <- ifelse(d$gender == "Masculino",1,0)
+
+# transforming the df to a matrix, without a few variables that don't matter
+d_caret <- d %>%
+  select(!c(Participant,
+            DomainNegativeAffect,
+            DomainDetachment,
+            DomainAntagonism,
+            DomainDisinhibition,
+            DomainPsychoticism,
+            PC,
+            ConfSD,
+            ReacTimeMean_DiscTask,
+            ReacTimeSD_DiscTask,
+            ReacTimeMean_ConfTask,
+            ReacTimeSD_ConfTask,
+            mc))
+
+### selecting the alpha and the lambda using caret
+loocv = trainControl(method = "LOOCV")
+
+m_elnet  = train(
+  ConfMean ~ ., data = d_caret,
+  method = "glmnet",
+  trControl = loocv,
+  tuneLength = 50
+)
+
+alpha_selected <- m_elnet$bestTune[[1]] # 0.2285714
+lambda_selected <- m_elnet$bestTune[[2]] # 0.1555395
+
+## runing the model regression with the selected alpha and lambda
+
+d_mat <- d %>%
+  select(!c(Participant,
+            DomainNegativeAffect,
+            DomainDetachment,
+            DomainAntagonism,
+            DomainDisinhibition,
+            DomainPsychoticism,
+            PC,
+            ConfMean,
+            ConfSD,
+            ReacTimeMean_DiscTask,
+            ReacTimeSD_DiscTask,
+            ReacTimeMean_ConfTask,
+            ReacTimeSD_ConfTask,
+            mc)) %>%
+  data.matrix()
+
+
+
+fit <- glmnet(d_mat, d$ConfMean, 
+              family = "gaussian",
+              alpha = alpha_selected,
+              lambda = lambda_selected)
+
+print(fit)
+
+a <- coef(fit)
+
+
+######## sigo probando ahora mas manual VA ESTE!!!
+library(reshape)
+
+beta <- coef(fit)
+
+tmp <- as.data.frame(as.matrix(beta))
+tmp$coef <- row.names(tmp)
+tmp <- reshape::melt(tmp, id = "coef")
+tmp$variable <- as.numeric(gsub("s", "", tmp$variable))
+tmp$lambda <- fit$lambda[tmp$variable+1] # extract the lambda values
+tmp$norm <- apply(abs(beta[-1,]), 2, sum)[tmp$variable+1] # compute L1 norm
+
+## aca si no es alguno de los siguientes poner gris en una col
+## sino poner algun otro color aleatorio
+## los que van en color:
+## Grandiosity, Hostility, Impulsivity, RestrictedAffectivity,
+## SeparationInsecurity, Submissiveness
+tmp_lala <- tmp %>%
+  mutate(color = (coef ==  "Grandiosity" |
+                    coef ==  "Hostility" |
+                    coef == "Impulsivity"|
+                    coef == "RestrictedAffectivity" |
+                    coef == "SeparationInsecurity" |
+                    coef == "Submissiveness")) %>%
+  mutate(color = (coef == "Grandiosity", "red")) #### CORREGIR ACA
+
+
+ggplot(tmp[tmp$coef != "(Intercept)",], 
+       aes(lambda, value, color = coef)) + 
+  geom_line(size = 1) + 
+  scale_x_log10() + 
+  xlab("Lambda (log scale)") + 
+  guides(color = guide_legend(title = ""), 
+         linetype = guide_legend(title = "")) +
+  geom_vline(xintercept= lambda_selected,
+             linetype='dashed', color='black', size=1) +
+  theme_bw() +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        plot.margin = margin(1, 1,1, 1, "cm"),
+        #legend.text =  element_text(size = 15),
+        panel.background = element_blank(),
+        #axis.text.x = element_text(size = 15,angle = 45, hjust=1),
+        #axis.text.y = element_text(size = 15),
+        #axis.title.x = element_blank(),
+        axis.title.y = element_blank())#+ 
+  #theme(legend.key.width = unit(3,"lines"))
+
+
+
+
+
+
+
+####################### HASTA ACA ESTA BIEN
+##### intento plotear como caen las variables en base al lambda
+library(plotmo)
+
+fit <- glmnet(d_mat, d$ConfMean, 
+              family = "gaussian",
+              alpha = alpha_selected)
+#,lambda = lambda_selected)
+
+plotres(fit, which = 1, id.n = 1)
+
+library(arm)
+library(coefplot)
+coefplot(fit, lambda=lambda_selected, sort='magnitude')
+coefpath(fit, showLegend = "never",
+         colorMin = "black",
+         strokePatternMin = "dotted") ############# WOW VA POR ACA
+
+
+plot(fit, xvar='lambda', label=TRUE)
+
+
+plot(fit)
+
+
+### now, I perform boostrap to know the CI and the proportion of times that the 
+### predictor was exactly 0.
+
+boot_fun <- function(data, indices, alpha_selected , lambda_selected, ConfMean){
+  boot_data <- data[indices,]
+  
+  boot_matrix <- boot_data %>%
+    select(!c(Participant,
+              DomainNegativeAffect,
+              DomainDetachment,
+              DomainAntagonism,
+              DomainDisinhibition,
+              DomainPsychoticism,
+              PC,
+              ConfMean,
+              ConfSD,
+              ReacTimeMean_DiscTask,
+              ReacTimeSD_DiscTask,
+              ReacTimeMean_ConfTask,
+              ReacTimeSD_ConfTask,
+              mc)) %>%
+    data.matrix()
+  
+  fit_boot <- glmnet(boot_matrix, ConfMean, 
+         family = "gaussian",
+         alpha = alpha_selected,
+         lambda = lambda_selected)
+  
+  coef_fit_boot <- coef(fit_boot) 
+  c <- c(coef_fit_boot[1:length(coef_fit_boot)]) 
+  
+  return(c) 
+  }
+
+rep_boot <- boot(
+    data = d,
+    statistic = boot_fun,
+    R = 5000, 
+    alpha_selected = alpha_selected,
+    lambda_selected = lambda_selected,
+    ConfMean = d$ConfMean
+  )
+
+boot.ci(rep_boot, index = 12) # se puede ir variando el index
+
+# probability of 0 (page 154, Statistical learning with sparsity... Hastie)
+sum(rep_boot$t[,12] == 0)/5000
+sum(rep_boot$t[,13] == 0)/5000
+sum(rep_boot$t[,14] == 0)/5000
+
+save(rep_boot, file = "git/Data/Regression_Results/Conf_PID_domain_Boot_elasticNet.RData")
+
+hist(rep_boot$t[,13])
+mean(rep_boot$t[,5])
+plot(rep_boot, index = 2)
+plot(rep_boot, index = 12)
+plot(rep_boot, index = 13)
+plot(rep_boot, index = 14)
--- git/Figures/Code/PlotSumms_severalModels_Metacognition.R
+++ git/Figures/Code/PlotSumms_severalModels_Metacognition.R
@@ -7,47 +7,130 @@ basename(getwd())
 # library
 library(jtools)
 library(tidyverse)
+library(lme4)
 
+
+### reg normal
+
+# cargo datos
 filepath <- root$find_file("git/Data/Regression_Results/mc_PID_domain_linear_model.RData")
 load(file= filepath)
 
 Linear_Model <- a
 
-### Regresion lineal de dominios para predecir mc por sujeto
+# extraigo la info
+sum_a <- summary(a)
+terms <-rownames(sum_a$coefficients)
+coeff <- unname(sum_a$coefficients[,"Estimate"])
+se <- unname(sum_a$coefficients[,"Std. Error"])
+model <- rep("normal", length(terms))
 
+# creo el df que va a guardar todo
+df.models <- data.frame(terms = terms,
+                 coeff = coeff,
+                 se = se,
+                 model = model)
+  
+### reg beta
+
+# cargo los datos
 filepath <- root$find_file("git/Data/Regression_Results/mc_PID_domain_Beta_linear_model.RData")
 load(file= filepath)
 
 Beta_Model <- a
 
+# extraigo info
+sum_a <- summary(a)
+terms <- rownames(sum_a$coefficients$mean)
+coeff <- unname(sum_a$coefficients$mean[,"Estimate"])
+se <- unname(sum_a$coefficients$mean[,"Std. Error"])
+model <- rep("beta", length(terms))
+
+df.beta <- data.frame(terms = terms,
+                        coeff = coeff,
+                        se = se,
+                        model = model)
+
+# uno con el df principal
+df.models <- rbind(df.models, df.beta)
+
+### reg log mixta
+
+# cargo los datos
+filepath <- root$find_file("git/Data/Regression_Results/mc_PID_domain_logistic_mixed_model.RData")
+load(file= filepath)
+
+log_mixed_Model <- a_log
+
+# extraigo info
+terms <-attr(a_log@pp$X, "dimnames")[[2]]
+terms <- c("(Intercept)","confidence_key.norm"  ,"DomainNegativeAffect.norm", "DomainDetachment.norm", "DomainAntagonism.norm", "DomainDisinhibition.norm", "DomainPsychoticism.norm", "gender", "age.norm")
+coeff <- a_log@beta
+sum_a_log <- summary(a_log)
+se <- unname(sum_a_log$coefficients[,"Std. Error"])
+model <- rep("log_mix", length(terms))
+
+# guardo en el df
+df.log_mix <- data.frame(terms = terms,
+                      coeff = coeff,
+                      se = se,
+                      model = model)
+
+# saco el predictor aislado de confianza
+df.log_mix <-  df.log_mix %>%
+  filter(!row_number() == 2)
+
+# uno con el df principal
+df.models <- rbind(df.models, df.log_mix)
+
+
+
+# saco la intercept que no es intepretable
+df.models <- df.models %>%
+  filter(terms != "(Intercept)")
+
+
 ## plot both models
 
-plot_summs(Linear_Model,
-           Beta_Model, 
-           coefs = c('Afecto Negativo' = 'DomainNegativeAffect.norm',
-                     'Desapego'='DomainDetachment.norm',
-                     'Antagonismo' = 'DomainAntagonism.norm',
-                     'Desinhibicion'='DomainDisinhibition.norm',
-                     'Psicoticismo'='DomainPsychoticism.norm',
-                     'genero' = 'gender',
-                     'edad' = 'age.norm') ,
-           model.names = c('Modelo normal', 'Modelo beta'),
-           plot.distributions = FALSE)+
-  ylab("") +
-  xlab("Coeficientes de la regresion") +
+############### intento 1
+
+ggplot(df.models, aes(x = terms, y = coeff)) + 
+  geom_pointrange(aes(ymax = coeff + se*2 , ymin = coeff - se*2, color = model)) +
+  geom_text(aes(label = coeff), nudge_x = 0.15) + 
+  scale_x_discrete("") + 
+  geom_hline(yintercept = 0, color = "red") + 
+  theme_bw() + 
+  theme(text = element_text(size=10)) +
+  ylab(NULL) + 
+  coord_flip() 
+#  facet_grid(~model)
+
+############### intento 2
+
+ggplot(df.models , aes(terms,coeff, color=model)) +
+  geom_point(aes(shape=model),size=4, 
+             position=position_dodge(width=0.2)) +
+  geom_hline(yintercept= 0, linetype='dashed', color= "black")+
+  scale_color_manual(name="Model",
+                     values=c("coral","steelblue", "red")) +
+  scale_shape_manual(name="Model",values=c(17,19, 18)) +
+  scale_y_continuous("Coefficent") +
+  geom_errorbar(aes(ymin= coeff - 2* se,ymax= coeff + 2* se),
+                width=0.1,
+                position=position_dodge(width=0.2))+
   theme_bw() +
   theme(axis.line = element_line(colour = "black"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        plot.margin = margin(1, 1,1, 1, "cm"),
-        legend.text = element_text(size=20),
-        legend.title = element_blank(),
-        panel.background = element_blank(),
-        axis.text.x = element_text(size = 30),
-        axis.text.y = element_text(size = 30),
-        axis.title.y = element_text(size = 30),
-        axis.title.x = element_text(size = 30))
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.border = element_blank(),
+      plot.margin = margin(1, 1,1, 1, "cm"),
+      legend.text =  element_text(size = 15),
+      panel.background = element_blank(),
+      axis.text.x = element_text(size = 15,angle = 45, hjust=1),
+      axis.text.y = element_text(size = 15),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank())
+
 
 ggsave("git/Figures/Figures/severalModels_Metacognicion.png", 
        width = 10, height = 6)
